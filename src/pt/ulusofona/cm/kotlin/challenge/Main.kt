ecdd012be6aa186ca274dc911c237d7898f43052
a22004247 <4124.g17.gfp@gmail.com>
2023-03-27 16:33:36 +0100

    Commit 11

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -4,7 +4,6 @@ import pt.ulusofona.cm.kotlin.challenge.models.*
 import java.util.*
 
 fun main() {
-    // Criar objetos de teste
     val posicao = Posicao(0,0)
     val motor = Motor(150, 2000)
     val carro = Carro("ABC123", motor)
@@ -12,19 +11,15 @@ fun main() {
     val carta = Carta()
     val pessoa = Pessoa("João", Date(2000, 1, 1))
 
-    // Adicionar veículos à pessoa
     pessoa.comprarVeiculo(carro)
     pessoa.comprarVeiculo(bicicleta)
 
-    // Testar métodos de movimento
     pessoa.moverVeiculoPara("ABC123", 10, 20)
     bicicleta.moverPara(5, 10)
 
-    // Testar pesquisa de veículo e venda
     val veiculo = pessoa.pesquisarVeiculo("ABC123")
     pessoa.venderVeiculo("ABC123", Pessoa("Maria", Date(1995, 2, 2)))
 
-    // Testar métodos de carta
     pessoa.tirarCarta()
     if (carro.requerCarta()) {
         try {
@@ -35,7 +30,6 @@ fun main() {
         }
     }
 
-    // Testar método toString
     println(carro.toString())
     println(bicicleta.toString())
     println(pessoa.toString())
--- src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Bicicleta.kt
@@ -1,7 +1,16 @@
 package pt.ulusofona.cm.kotlin.challenge.models
+import java.text.SimpleDateFormat
 
 class Bicicleta(override val identificador: String) : Veiculo(identificador) {
     override fun requerCarta(): Boolean {
         return false
+    }
+    private fun formatarData() : String {
+        val formatoData = SimpleDateFormat("dd-MM-yyyy")
+        val dataModificada = formatoData.format(dataDeAquisicao)
+        return dataModificada.toString()
+    }
+    override fun toString(): String {
+        return "Bicicleta | $identificador | ${formatarData()} | $posicao"
     }
 }
